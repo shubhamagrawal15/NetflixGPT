f196af226beb92cf45a1612fcb8364fc0f1af3b1
= <shubhamagrawal290304@gmail.com>
2025-12-05 10:05:48 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# NETFLIX GPT
+
+- Create vite @latest
+- configured tailwindCSS
+
+# Features
+
+- Browse
+  - Header
+  - Main Movie
+    - Trailer in Background
+    - Title and Description
+    - Movie Suggestions
+      - Movies List \* n (vertically scrolled)
---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='text-3xl text-center font-bold text-green-800' >
+      <h1>Namaste everyone!! lets build netflix gpt</h1>
+    </div>
+  )
+}
+
+export default App
